0491fa4e0c8882a322f746f36e8c8fb45df74984
infinityplus1 <the.infinityplus1@gmail.com>
2022-04-09 22:20:44 +0100

    added aws aliases

--- aliases.zsh
+++ aliases.zsh
@@ -2,3 +2,9 @@ alias psme='ps f -fU $USER'
 alias psroot='ps f -fU root'
 alias cls='clear'
 alias CLS='clear'
+alias p='podman'
+alias docker='podman'
+
+# AWS specific aliases
+alias aws.login='saml2aws login -a work-dev'
+alias aws.whoami='aws sts get-caller-identity --no-cli-pager'
