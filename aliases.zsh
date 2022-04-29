5f1cfb3a5fd4fc74282aa4e5ddf4fc29439e2066
Tyn Ong <Tyn.Ong@woodmac.com>
2022-04-29 14:34:40 +0100

    new aliases

--- aliases.zsh
+++ aliases.zsh
@@ -4,7 +4,16 @@ alias cls='clear'
 alias CLS='clear'
 alias p='podman'
 alias docker='podman'
+alias path='echo $PATH'
+
 
 # AWS specific aliases
 alias aws.login='saml2aws login -a work-dev'
 alias aws.whoami='aws sts get-caller-identity --no-cli-pager'
+
+# S3
+alias s3='aws s3'
+
+
+# Cloudformation
+alias cfn='aws cloudformation'
